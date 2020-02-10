07c78de3399e9e470fafef330a38ac48db3061d5
Adam Neulight <arn8435@g.rit.edu>
2020-02-10 12:23:10 -0500

    initial commit

---
+++ led.py
@@ -0,0 +1,9 @@
+import board
+import neopixel
+import time
+pixels = neopixel.NeoPixel(board.D18, 120, brightness=1)
+for i in range(10):
+    pixels.fill((255, 255, 255))
+    time.sleep(5)
+    pixels.fill((0, 0, 0))
+    time.sleep(1) 
